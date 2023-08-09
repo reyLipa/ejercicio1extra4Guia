4f00ae4ad8ad2dced181790f1c694b78c4164008
lulumato <luciamatonti390@gmail.com>
2023-08-09 18:57:55 -0300

    haber

---
+++ src/ejercicio1extra4guia/ClaseLucia.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1extra4guia;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ClaseLucia {
+    
+   public void saludo(){
+       System.out.println("HOLA COMPAS");}
+}
