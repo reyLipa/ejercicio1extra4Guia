f8e450e9a4cd58e5c6a9297b61b6f99da5b634a6
AmadoEstefania <esttefania10@gmail.com>
2023-08-09 19:00:25 -0300

    Nuevos atributos y constructor

--- src/ejercicio1extra4guia/ClaseAngela.java
+++ src/ejercicio1extra4guia/ClaseAngela.java
@@ -7,10 +7,38 @@ public class ClaseAngela {
     private int numero;
     private String nombre; 
     private String apellido; 
+    private int telefono; 
 
-    public ClaseAngela(int numero, String nombre, String apellido) {
+    public ClaseAngela(int numero, String nombre, String apellido, int telefono) {
         this.numero = numero;
         this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+   
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
         this.apellido = apellido;
     }
     
